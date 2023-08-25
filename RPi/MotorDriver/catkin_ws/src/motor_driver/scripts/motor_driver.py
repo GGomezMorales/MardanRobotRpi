422e4447c86f14544f04bf9ec541a4703a28e3c7
GGomezMorales <gugomog3@gmail.com>
2023-08-25 14:27:17 -0500

    fix: motor_controller

--- RPi/MotorDriver/catkin_ws/src/motor_driver/scripts/motor_driver.py
+++ RPi/MotorDriver/catkin_ws/src/motor_driver/scripts/motor_driver.py
@@ -4,23 +4,50 @@ import rospy
 from geometry_msgs.msg import Twist
 
 import zumo_lib as motors
+import threading
+
+class MockMotorController:
+    def __init__(self):
+        pass
+
+    def send_speed(self, speed_l, speed_r, log = False):
+        print(f'speed_l = {speed_l}, speed_r = {speed_r}')
+
+class I2CMotorController:
+    def __init__(self):
+        self.motors.init_i2c_bus()
+
+    def send_speed(self, speed_l, speed_r, log = False):
+        self.motors.send_speed(speed_l, speed_r)
 
 class MotorController:
-    def __init__(self):
-        self.sub = rospy.Subscriber("motors/motor_twist", Twist, self.callback)
+    def __init__(self, motor_interface):
+        self.sub = rospy.Subscriber("motors/motor_twist", Twist, self.data_processing_callback)
         self.vel_max = 200
-        motors.init_i2c_bus()
+        self.l_val = 0
+        self.r_val = 0
+        self.rate = rospy.Rate(10)
+        self.worker = threading.Thread(target=self.i2c_thread)
+        self.motor_interface = motor_interface
 
-    
-    def callback(self, twist):
-        l_val = int((twist.linear.x - twist.angular.z)*self.vel_max/2)
-        r_val = int((twist.linear.x + twist.angular.z)*self.vel_max/2)
-        motors.send_speed(l_val, r_val)
+    def data_processing_callback(self, twist):
+        self.l_val = int((twist.linear.x - twist.angular.z)*self.vel_max/2)
+        self.r_val = int((twist.linear.x + twist.angular.z)*self.vel_max/2)
 
+    def i2c_thread(self):
+        while not rospy.is_shutdown():
+            motors.send_speed(self.l_val, self.r_val)
+            self.rate.sleep()
+
+    def star_worker(self):
+        self.worker.start()
 
 def main():
+    ## i2cMotorController = I2CMotorController()
+    i2cMotorController = MockMotorController()
     rospy.init_node('motor_driver')
-    obc = MotorController()
+    motorController = MotorController(i2cMotorController)
+    motorController.star_worker()
     try:
         rospy.spin()
     except KeyboardInterrupt:
@@ -29,3 +56,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
